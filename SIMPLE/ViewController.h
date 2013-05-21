8b46ad4d3de652cd50e0ec375842d7a0a35a0676
Rohan Sinha <rsinha1994@gmail.com>
2013-05-21 17:01:56 +0530

    Initial Commit

---
+++ SIMPLE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SIMPLE
+//
+//  Created by Rohan Sinha on 21/05/13.
+//  Copyright (c) 2013 Rohan Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SIMPLE/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SIMPLE
+//
+//  Created by Rohan Sinha on 21/05/13.
+//  Copyright (c) 2013 Rohan Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
